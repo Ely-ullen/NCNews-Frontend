3dce11c9fa7498a9304356689df6ecd98e6dd620
Ely-ullen <eleanor.p.bradley@gmail.com>
2022-08-24 14:04:15 +0100

    completed the votes task

---
+++ src/components/Votes.js
@@ -0,0 +1,47 @@
+import "../CSSComponets/votes.css";
+import { patchVotes } from "../api";
+import { useEffect, useState } from "react";
+import { fetchVotes } from "../api";
+
+const Votes = ({ articleId }) => {
+  const [voteCount, setVoteCount] = useState(0);
+  const [click, setClick] = useState(true);
+  const [buttonName, setButtonName] = useState("false");
+  useEffect(() => {
+    fetchVotes(articleId).then((votes) => {
+      setVoteCount(votes);
+    });
+  }, [articleId]);
+
+  const clickFunc = () => {
+    setClick(!click);
+    return true;
+  };
+
+  async function handleClick() {
+    await clickFunc();
+
+    if (click) {
+      setVoteCount((currVotes) => currVotes + 1);
+      setButtonName("true");
+      patchVotes({ inc_votes: +1 }, articleId);
+    }
+    if (click === false) {
+      setVoteCount((currVotes) => currVotes - 1);
+      setButtonName("false");
+      patchVotes({ inc_votes: -1 }, articleId);
+    }
+  }
+
+  return (
+    <>
+      <span>Votes:{voteCount} </span>
+
+      <button className={buttonName} onClick={handleClick}>
+        Vote!
+      </button>
+    </>
+  );
+};
+
+export default Votes;
